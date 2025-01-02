a3c4241139217af3bcf4d499f22235c12b98f036
hellosiyam <hellosiyambd@gmail.com>
2025-01-02 23:16:23 +0600

    complite sort.js

---
+++ sort.js
@@ -0,0 +1,33 @@
+const person=["rakib", "pakib", "dakib", "akib", "sakip", "nokib"];
+// console.log(person.sort());
+
+// const number= [5, 10, 15, 65, 14, 21, 0, 32, 9];
+// const number_a = [...number].sort(function (a, b) {return a - b})
+// const number_d = [...number].sort(function (a, b) {return b - a})
+// console.log(number_a);
+// console.log(number_d);
+
+// const number= [5, 10, 15, 65, 14, 21, 0, 32, 9];
+// const number_a=[...number].sort(function (a, b) {return a-b})
+// const number_d=[...number].sort(function (a, b) {return b-a})
+// console.log(number_a);
+// console.log(number_d);
+
+// const number = [5, 15, 12, 14, 5,45, 8,9, 78, 5, 4,]
+// const number_a = number.sort(function (a, b) {return a-b})
+// const number_d = number.sort(function (a,b) {return b-a})
+
+// console.log(number_a);
+// console.log(number_d);
+
+const number=[5, 45, 84 ,75, 12, 32, 21 ,58, 78, 96, 100]
+const number_a=number.sort(function (a,b) {return a-b})
+const number_d=number.sort(function (a,b) {return b-a})
+console.log(number_a);
+console.log(number_d);
+
+
+
+
+
+
