c36632a9d3280de51bb26c4a4a2c8acdb7168b94
hellosiyam <hellosiyambd@gmail.com>
2025-01-02 22:47:18 +0600

    complite reversed.js

---
+++ reversed.js
@@ -0,0 +1,25 @@
+// const num=[1, 2, 3, 4, 5, 6, 7, 8, 9, ]
+// const rev=num.reverse();
+// console.log(rev);
+
+// const nam=["Elon", "Bill", "Mark", "Jajos"];
+// // const rever=nam.reverse();
+// console.log(nam.reverse());
+
+// const nam=["Elon", "Bill", "Mark", "Jajos"];
+// const rev=[];
+// for(const na of nam){
+//     // console.log(na);
+//     rev.unshift(na);
+// }
+// console.log(rev);
+
+const nam=["Elion", "Bilo", "Micro", "Joserf"]
+const rev =[];
+for(const na of nam){
+    rev.unshift(na)
+}
+console.log(rev);
+
+
+
