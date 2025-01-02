dfb88e4e41b515e5bbf0bdaeaf4675d6dfaabadd
hellosiyam <hellosiyambd@gmail.com>
2025-01-03 00:05:47 +0600

    complite task.js

---
+++ task.js
@@ -0,0 +1,56 @@
+// Task-1
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const rev=[];
+// for(const color of colors){
+//     rev.unshift(color);
+// }
+// console.log(rev);
+
+// Task-2
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const even=[];
+// for(i=0; i<numbers.length; i++){
+//     if (numbers[i]%2===0) {
+//         even.push(numbers[i]);
+//     }
+// }
+// console.log(even);
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const even = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 ===0) {
+//         even.push(numbers[i])
+//     }
+// }
+// console.log(even);
+
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const even = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i]%2===0) {
+//         even.push(numbers[i])
+//     }
+// }
+// console.log(even);
+
+// Task-3
+// let numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+// let result = [];
+// for(let element of numbers){
+//     result=element+result;
+// }
+// console.log(result);
+
+// let numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+// let add= [];
+// for(let element of numbers){
+//     add=element+add;
+// }
+// console.log(add);
+
+// Task-4
+const statement = 'I am a hard working person'
+const rev = statement.split(" ").reverse().join(" ")
+console.log(rev);
+
+
