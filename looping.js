417369d29d3eb9bd6be659bdc334fa1d1eac1dbf
hellosiyam <hellosiyambd@gmail.com>
2025-01-02 22:18:38 +0600

    Complite Looping

---
+++ looping.js
@@ -0,0 +1,18 @@
+// let friends = ["Elon", "Bill", "Mark", "Jasos"];
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(i);
+//     console.log(friends[i]);    
+// }
+
+let friends = ["Elon", "Bill", "Mark", "Bazos"];
+for (let i = 0; i < friends.length; i++) {
+    console.log(i);
+    console.log(friends[i]);
+    
+}
+// let i=0
+// while (i<friends.length) {
+//     console.log(friends[i]);
+//     i++
+// }
+
