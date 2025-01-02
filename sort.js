a7a6a0e93a2fb50b8c46a7a3355196c28f956075
hellosiyam <hellosiyambd@gmail.com>
2025-01-02 23:17:36 +0600

    complite sort.js

--- sort.js
+++ sort.js
@@ -21,8 +21,8 @@ const person=["rakib", "pakib", "dakib", "akib", "sakip", "nokib"];
 // console.log(number_d);
 
 const number=[5, 45, 84 ,75, 12, 32, 21 ,58, 78, 96, 100]
-const number_a=number.sort(function (a,b) {return a-b})
-const number_d=number.sort(function (a,b) {return b-a})
+const number_a=[...number].sort(function (a,b) {return a-b})
+const number_d=[...number].sort(function (a,b) {return b-a})
 console.log(number_a);
 console.log(number_d);
 
